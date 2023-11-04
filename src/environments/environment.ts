15327cfb7804e8011b22fa243075784a3aadc527
William Duan <william.duan@g.com>
2023-11-04 17:21:55 +1100

    environment of firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,11 +5,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyASCxHgCSpmVnQwVbLCHATcns5fJQFdmpY",
-    authDomain: "clips-36548.firebaseapp.com",
-    projectId: "clips-36548",
-    storageBucket: "clips-36548.appspot.com",
-    appId: "1:798864185458:web:f93bd21de7f956bffc4aeb"
+    apiKey: "AIzaSyBs9zpcX2MuHQvEQo8DRs2_8PFljrBAsJs",
+    authDomain: "clips-william.firebaseapp.com",
+    projectId: "clips-william",
+    storageBucket: "clips-william.appspot.com",
+    appId: "1:449293413482:web:57bd383b1e41b10b42ee13"
   }
 };
 
